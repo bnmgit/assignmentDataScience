9d750c2ef1d40f231a3f87b3e11c21ba02aa8ba2
bnmgit <badri.hadoop@gmail.com>
2014-04-28 13:35:56 -0400

    first commit

---
+++ assignmentsDataScience/1-ds_toolbox/week-1_Quiz.R
@@ -0,0 +1,39 @@
+## Week-1_Quiz ##
+
+## 1. Which of the following are courses in the Data Science Specialization? 
+## Select all that apply.
+
+## Business Analytics  - False	
+## Developing Data Products	- Correct		
+## Exploratory Data Analysis - Correct	
+## The Elements of Statistical Learning	- False
+
+## 2. Why are we using R for the course track? Select all that apply.
+
+## R has a large number of add on packages that are useful for data analysis - Correct		
+## R is one of the most widely used programming languages for Data Science - Correct		
+## R is a general purpose programming language - False	
+## R is the best cloud computing language - False
+
+## 3. What are good ways to find answers to questions in this course track? Select all that apply.
+
+##Posting to the Course Message Boards - Correct  
+##Emailing your instructors	- False	
+##Posting homework assignments to mailing lists	- False	
+##Searching Stack Overflow - Correct	
+
+## 4. What are characteristics of good questions on the message boards? Select all that apply.
+
+## Provides no details. - False	
+## Is very specific. - Correct		
+## Describes the goal of the analysis - Correct	
+## Begs for help without providing information - False	
+
+## 5. Which of the following packages provides machine learning functionality? Select all that apply
+
+## caret - Correct		
+## filehash - false	
+## knitr - false	
+## rpart - Correct		
+
+
